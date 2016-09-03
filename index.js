3a6ba2484434be8d1cb6d366b8ba099a63984a6f
Dan Finlay <dan@danfinlay.com>
2016-09-02 20:01:14 -0700

    Check for Ethereum web3 compatible environment.

--- index.js
+++ index.js
@@ -7,14 +7,31 @@ var body = document.querySelector('body')
 const container = document.createElement('div')
 body.appendChild(container)
 
+let web3Found = false
+window.addEventListener('load', function() {
+
+  // Checking if Web3 has been injected by the browser (Mist/MetaMask)
+  if (typeof web3 !== 'undefined') {
+
+    // Use Mist/MetaMask's provider
+    web3Found = true
+  }
+
+  // Now you can start your app & access web3 freely:
+  startApp()
+})
+
 const store = configureStore({
   currentView: 'home',
   nonce: 1,
+  web3Found,
 })
 
-render(
-  h(Root, {
-    store,
-  }),
-container)
+function startApp(){
+  render(
+    h(Root, {
+      store,
+    }),
+  container)
+}
 

22751b153d1842a741c0ebd6242437fa3a7715c2
Dan Finlay <dan@danfinlay.com>
2017-02-10 04:08:06 -0800

    Got working with EthJS

--- app/root.js
+++ app/root.js
@@ -6,10 +6,7 @@ const connect = require('react-redux').connect
 module.exports = connect(mapStateToProps)(AppRoot)
 
 function mapStateToProps (state) {
-  return {
-    view: state.currentView,
-    nonce: state.nonce,
-  }
+  return state
 }
 
 inherits(AppRoot, Component)
@@ -19,6 +16,7 @@ function AppRoot () {
 
 AppRoot.prototype.render = function () {
   const props = this.props
+  const { eth, loading, nonce, error, web3Found } = props
 
   return (
     h('.content', [
@@ -27,20 +25,54 @@ AppRoot.prototype.render = function () {
           background: 'grey',
         },
       }, [
-        h('h1', `Welcome ${props.view}`),
-        h('h2', `The count is ${props.nonce}`),
 
-        h('button', {
-          onClick: () => this.incrementNonce(),
-        }, 'COUNT HIGHER!'),
+        h('h1', `The MetaMask Stack`),
+
+        h('h3', [
+          'A quick way to start building Web Dapps on ',
+          h('a', {
+            href: 'https://ethereum.org/'
+          }, 'Ethereum'),
+        ]),
+
+        web3Found ?
+          h('div', [
+            h('You should get MetaMask for the full experience!'),
+            h('img', {
+              src: 'http://www.microtick.com/images/download-metamask.png',
+            }),
+          ])
+          : loading ? h('span', 'Loading...') : h('button', {
+            onClick: () => this.sendTip(),
+          }, 'Tip the developer with Ethereum'),
+
+        h('br'),
+        nonce > 0 ? h('h2', `Thanks for your generous ${nonce} tip!`) : null,
+        h('br'),
+        error ? h('span', { style: { color: '#fbfbfb' } }, error) : null,
 
       ])
     ])
   )
 }
 
-AppRoot.prototype.incrementNonce = function() {
-  this.props.dispatch({
-    type: 'INCREMENT_NONCE'
+AppRoot.prototype.sendTip = function() {
+  const { eth } = this.props
+
+  this.props.dispatch({ type: 'SHOW_LOADING' })
+  eth.sendTransaction({
+    value: '1',
+    to: '0x55e2780588aa5000F464f700D2676fD0a22Ee160',
   })
+  .then((result) => {
+    this.props.dispatch({ type: 'HIDE_LOADING' })
+    this.props.dispatch({
+      type: 'INCREMENT_NONCE',
+    })
+  })
+  .catch((reason) => {
+    this.props.dispatch({ type: 'HIDE_LOADING' })
+    this.props.dispatch({ type: 'SHOW_ERROR', value: reason.message })
+  })
+
 }
--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const render = require('react-dom').render
 const h = require('react-hyperscript')
 const configureStore = require('./lib/store')
 const Root = require('./app/root.js')
+const Eth = require('ethjs');
+let eth;
+
 
 var body = document.querySelector('body')
 const container = document.createElement('div')
@@ -15,16 +18,23 @@ window.addEventListener('load', function() {
 
     // Use Mist/MetaMask's provider
     web3Found = true
+    eth = new Eth(web3.currentProvider)
+  } else {
+
+    // Fall back to Infura:
+    eth = new Eth(new Eth.HttpProvider('https://mainnet.infura.io'))
   }
+
+  store.dispatch({ type: 'ETH_LOADED', value: eth })
 
   // Now you can start your app & access web3 freely:
   startApp()
 })
 
 const store = configureStore({
-  currentView: 'home',
-  nonce: 1,
+  nonce: 0,
   web3Found,
+  web3,
 })
 
 function startApp(){
